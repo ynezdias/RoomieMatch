f62f6fedd2568deb76028e5e751a9c65f3343b7c
Ynez Dias <ynezdias0501@gmail.com>
2026-01-27 16:59:21 -0500

    Added expo mobile ip address

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from 'expo-router'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { AuthProvider } from '../src/context/AuthContext'
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </AuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <Stack screenOptions={{ headerShown: false }} />
+      </AuthProvider>
+    </GestureHandlerRootView>
   )
 }
--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,17 +1,41 @@
+// import axios from 'axios'
+// import AsyncStorage from '@react-native-async-storage/async-storage'
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+// })
+
+// API.interceptors.request.use(async (config) => {
+//   const token = await AsyncStorage.getItem('token')
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+
+// export default API
+// //edited
+
 import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Platform } from 'react-native'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: Platform.select({
+    web: 'http://localhost:5000/api',
+    default: 'http://192.168.1.159:5000/api', // 👈 your Expo IP
+  }),
 })
 
-API.interceptors.request.use(async (config) => {
-  const token = await AsyncStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
-})
+API.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
 
 export default API
-//edited
